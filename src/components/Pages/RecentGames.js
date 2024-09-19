763971fde20bd57dd0c1f80b0b07923242d54ded
Aidan Murray <iiiaiid4niii@gmail.com>
2024-09-19 15:06:33 +0000

    working on recent games page formatting

--- src/components/Pages/RecentGames.js
+++ src/components/Pages/RecentGames.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import '../../App.css'
 import { getGames } from '../../ApiFuncs';
+import { Link } from 'react-router-dom';
 
 const RecentGames = () => {
     const [games, setGames] =useState([])
@@ -12,12 +13,20 @@ const RecentGames = () => {
     },[])
 
     return (
-        <div className='RecentGames'>
-            {games.map((game, index) => {
-                <div>
+        <div className="MainContainer">
+        <Link to="/" className="HomeLink">
+          <h1 className="TitleHeader">App</h1>
+        </Link>
+        <h2>Recent games</h2>
+        <div className="pageContainer">
+            {games.map((game, index) => (
+                <div className='recentGameCard' key={index}>
+                    <p>Game Date: {new Intl.DateTimeFormat('en-GB').format(new Date(game.date))}</p>
+                    <p>Game ID: {game.id}</p>
                 </div>
-            })}
+            ))}
         </div>
+      </div>
     );
 };
 
