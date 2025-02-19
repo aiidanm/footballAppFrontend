4656c25445e3de3c8717ca9feb76fc822f5936bc
AidanMurray <iiiaiid4niii@gmail.com>
2025-02-19 22:21:38 +0000

    working on submit page more

--- src/components/submitPreview.jsx
+++ src/components/submitPreview.jsx
@@ -8,32 +8,61 @@ const SubmitPreview = ({
   overTheFence,
   handleSubmit,
 }) => {
+  useEffect(() => {
+    console.log(selectedPlayers);
+  });
 
-useEffect(()=>{
-    console.log(selectedPlayers)
-})
+  const redTeam = Object.values(selectedPlayers).filter(
+    (player) => player.team === "team1"
+  );
 
-  const filteredPlayers = Object.values(selectedPlayers).filter(
-    (player) => player.team === "team1" || player.team === "team2"
+  const blueTeam = Object.values(selectedPlayers).filter(
+    (player) => player.team === "team2"
   );
 
   return (
     <div>
       <h2>Confirm Selection</h2>
-      {filteredPlayers.map((player) => {
+      <div className="red_team_container">
+        <h3>Red Team</h3>
+        {redTeam.map((player) => {
         const currentSelection = player || {};
-        return (
-          <div
-            key={player.id}
-            className={`playerCard-${currentSelection.team || "unselected"}`}
-          >
-            <h3>
-              {player.name}
-            </h3>
-            <p>{player.goals_scored || 0}</p>
-          </div>
+        return (<table>
+          <tr>
+                      <th>Name</th>
+                      <th>Goals Scored</th>
+                      <th>Over the fence</th>
+                    </tr>
+          <tr>
+                          <td>{player.name}</td>
+                          <td>{player.goals_scored || 0}</td>
+                          <td>{player.times_over_fence || 0}</td>
+                        </tr>
+        </table>
         );
       })}
+      </div>
+      <div className="blue_team_container">
+        <h3>blue Team</h3>
+        {blueTeam.map((player) => {
+        const currentSelection = player || {};
+        return (
+          <table>
+          <tr>
+                      <th>Name</th>
+                      <th>Goals Scored</th>
+                      <th>Over the fence</th>
+                    </tr>
+          <tr>
+                          <td>{player.name}</td>
+                          <td>{player.goals_scored || 0}</td>
+                          <td>{player.times_over_fence || 0}</td>
+                        </tr>
+        </table>
+        );
+      })}
+      </div>
+      
       <button onClick={handleSubmit}>Confirm & Submit</button>
     </div>
   );
