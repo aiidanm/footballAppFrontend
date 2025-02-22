70698f5714edb0f632c61fc408ed6e791943de2c
AidanMurray <iiiaiid4niii@gmail.com>
2025-02-22 20:00:06 +0000

    changes to the preview page when submitting a game

--- src/components/submitPreview.jsx
+++ src/components/submitPreview.jsx
@@ -24,45 +24,48 @@ const SubmitPreview = ({
     <div>
       <h2>Confirm Selection</h2>
       <div className="red_team_container">
-        <h3>Red Team</h3>
-        {redTeam.map((player) => {
-        const currentSelection = player || {};
-        return (<table>
-          <tr>
-                      <th>Name</th>
-                      <th>Goals Scored</th>
-                      <th>Over the fence</th>
-                    </tr>
-          <tr>
-                          <td>{player.name}</td>
-                          <td>{player.goals_scored || 0}</td>
-                          <td>{player.times_over_fence || 0}</td>
-                        </tr>
-        </table>
-        );
-      })}
+        <h3>red Team</h3>
+            <table>
+              <tr>
+                <th>Name</th>
+                <th>Goals Scored</th>
+                <th>Over the fence</th>
+              </tr>
+              {redTeam.map((player) => {
+                const currentSelection = player || {};
+                return (
+                  <tr>
+                    <td>{player.name}</td>
+                    <td>{player.goals_scored || 0}</td>
+                    <td>{player.times_over_fence || 0}</td>
+                  </tr>
+                );
+              })}
+            </table>
+        
       </div>
       <div className="blue_team_container">
         <h3>blue Team</h3>
-        {blueTeam.map((player) => {
-        const currentSelection = player || {};
-        return (
-          <table>
-          <tr>
-                      <th>Name</th>
-                      <th>Goals Scored</th>
-                      <th>Over the fence</th>
-                    </tr>
-          <tr>
-                          <td>{player.name}</td>
-                          <td>{player.goals_scored || 0}</td>
-                          <td>{player.times_over_fence || 0}</td>
-                        </tr>
-        </table>
-        );
-      })}
+            <table>
+              <tr>
+                <th>Name</th>
+                <th>Goals Scored</th>
+                <th>Over the fence</th>
+              </tr>
+              {blueTeam.map((player) => {
+                const currentSelection = player || {};
+                return (
+                  <tr>
+                    <td>{player.name}</td>
+                    <td>{player.goals_scored || 0}</td>
+                    <td>{player.times_over_fence || 0}</td>
+                  </tr>
+                );
+              })}
+            </table>
+        
       </div>
-      
+
       <button onClick={handleSubmit}>Confirm & Submit</button>
     </div>
   );
